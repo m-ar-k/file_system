0ef6dc743a7c98404ee9010acaadcef1ed7eab34
m-ar-k <38901192+m-ar-k@users.noreply.github.com>
2021-02-13 14:02:20 -0800

    Update File.c

--- io/File.c
+++ io/File.c
@@ -38,7 +38,6 @@ int* checkFreeBlockBuffer(unsigned char* freeBlocks, int numBlocks) {
 
         if(vect==0) {
             i = j + 1;
-            //printf("II:    %d\n",i);
             break;
         }
     }
@@ -75,11 +74,7 @@ int createNewInode(int type, int* blocks, int blocknum, int size, unsigned char*
     // Blocks not yet allocated
     memcpy(writeBuffer, &size, sizeof(int));
     memcpy(writeBuffer+4, &type, sizeof(int));
-    //memset(&buffer6[(INODE_SIZE*(i%8))+sizeof(int)*2], 0, 24); // Set remaining memory to 0 since blocks have not yet been allocated
-    //memcpy(buffer6+(INODE_SIZE*(i%8))+sizeof(int)*2, &try, 2); /* try is where first block location should be */
     for (int j=0; j<blocknum; j++) memcpy(writeBuffer+8+(j*2), &blocks[j], 2); // Write block locations
-    
- 
     return i; // The position in the inode map
 }
 
@@ -93,56 +88,25 @@ void createNewDirectory(char* name, int type, int inode_num, int level, unsigned
    
     memcpy(directoryBuffer, &inode_num, 1); // 1st byte is inode number
     memcpy(directoryBuffer+1, file_name, 31); // Next 31 bytes are the name
-    //writeBlock(blockWrite, i, buffer7);
-
     free(file_name);
-    //free(buffer7);
-
 }
 
-// Update inode with block locations?
+// Update inode with block locations
 void createNewFlatFile(char* contents, char* name, int inodeRef, unsigned char* fileBuffer, int* blockLocations) { // pos is the first block allocated to start writing file data to
-    /*
-    FILE * fileWrite = fopen(vdisk_path, "rb+");
-    unsigned char * buffer8;
-    buffer8 = calloc(BLOCK_SIZE, 1);
-    */
-
     int len = strlen(contents);
     int blocks = ceil((double)len/(double)BLOCK_SIZE);
     
     // Figure out filepath and/or which directory to put file in?
     // Update Inode with block locations
     if (blocks < 2) {
-        /*
-        int i=18;
-        for(;i<NUM_BLOCKS;i++) if(checkFreeBlocks(i)==1) break;
-        flipBlockVector(i);
-        */
         strncpy((char*)fileBuffer, contents, len); 
-        //writeBlock(fileWrite, i, buffer8);
     }
     else {
-        /*
-        int* block_locations = (int*)malloc(sizeof(int)*blocks); // Store found locations of free blocks
-        int j=18; // Offset for reserved blocks
-        for(int i=0; i<blocks; i++) { // Loop through data and write to blocks
-            // Try to allocate all other blocks needed
-            for(;j<NUM_BLOCKS;j++) if(checkFreeBlocks(j)==1) break;
-            if (j == 128) printf("Error: failed to allocate enough blocks for new file\n"); // Return error if there is not enough space availible
-            block_locations[i] = j;  
-            j++;
-        }
-        */
         for (int i=0; i<blocks; i++) {
-            //flipBlockVector(block_locations[i]); // Set found free location to occupied
             memset(fileBuffer+(i*BLOCK_SIZE), 0, BLOCK_SIZE);
             strncpy((char*)fileBuffer+(i*BLOCK_SIZE), contents+(BLOCK_SIZE*i), BLOCK_SIZE);
-            //writeBlock(fileWrite, block_locations[i], buffer8);
         }
     }
-    //free(buffer8);
-    //fclose(fileWrite);
 }
 
 int findLevel(char* path) { // Return depth of path, if path does not start with '/' then return 0 which denotes an error
@@ -155,22 +119,17 @@ int findLevel(char* path) { // Return depth of path, if path does not start with
     return levels;
 }
 
-// Modification
-
 void addToDirectory(int inodeNum, int directoryRef, int dirSize, char* filename, unsigned char* writeBuffer, unsigned char* checkBuffer, int* buffBlocks, int numBlocks, unsigned char* freeBlockBuffer) { //Inode for new file, parent directory data block, parent directory current size, name of file being added
     
     FILE * readDir = fopen(vdisk_path, "rb+");
     readBlock(readDir, directoryRef, writeBuffer);
     fclose(readDir);
-
     unsigned char* dirBuff = writeBuffer;
-    //printf("%d\n", j);
 
     int i = 1;
     for (; i<16; i++) if ((int)*(dirBuff+(i*32))==0) break;
     memcpy(dirBuff+(i*32), &inodeNum, 1);
     strncpy((char*)dirBuff+(i*32)+1, filename, 31);
-    //printf("%s\n",writeBuffer+(j*BLOCK_SIZE)+(i*32)+1);
     dirSize += 32;
 
     memcpy(checkBuffer+(inodeNum*4), &buffBlocks[numBlocks], 4); // Updating the inode map for new inode location, special case for root?
@@ -203,12 +162,10 @@ int traverseFileTreeCheck(FILE* fp, unsigned char* inodesBuffer, unsigned char* 
     dir = strtok(rath, "/");
 
     if(!dir) dir = filename;
-    //printf("%s\n",dir); //debug
     if (level==1) parentNode = 1;
     
     for (int j=0; j<level; j++) {
         foundFile = checkIfFileIsInDirectory((char*)rootDir, dir); // Find the block reference to the next directory in the path
-        //printf("%d\n",foundFile);
         
         if (foundFile == 0) {
             if(j < level-1) printf("Error: Parent folder does not exist");
@@ -216,14 +173,7 @@ int traverseFileTreeCheck(FILE* fp, unsigned char* inodesBuffer, unsigned char* 
         }
         else {
             memset(rootDir, 0, BLOCK_SIZE);
-            //readBlock(fp, (int)*(inodesBuffer+((foundFile-1)*INODE_SIZE)+8), rootDir); // Assuming that next file is a directory, otherwise should check type flag at +4
-                                                                                         // Maybe need to load in more inodes into checkbuffer (total of 8 blocks)
-                                                                                         //ISSUE: Inode has not been allocated for this directory
             int dirPos = (int)*(inodesBuffer+(foundFile*4)); //Find location in inode checkpoint (*4 since each entry has 4 bytes)
-            //int dirBlock = floor((double)dirPos/(double)512);
-            //int dirBlockPos = dirPos%dirBlock; // Handling block location based on address to true location if there are multiple inodes per block
-           
-            //int nextDir = (int)*(rootDir+8);
             if (j==level-2) parentCheckpoint = foundFile;
             parentNode = foundFile;
             parentInode = dirPos;
@@ -233,32 +183,23 @@ int traverseFileTreeCheck(FILE* fp, unsigned char* inodesBuffer, unsigned char* 
             readBlock(fp, *(temp+8), rootDir);
             parentRef = *(temp+8);
             free(temp);
-            //parentRef = nextDir;
             if (j==level) printf("The file already exits\n");
-            //parentRef = (int)*(inodesBuffer+((foundFile-1)*INODE_SIZE)+8);
-            //printf("Parent: %d\n",(int)*(inodesBuffer+((foundFile-1)*INODE_SIZE)+8));
             }
             dir = strtok(NULL, "/");
         }
         return parentRef;
-        //return parentRef;
-        
 }
 
 char* findName(char* path, int level) {
-    //char* name = strtok(path, "/");
-    //printf("path: %s\t%lu\n",path,strlen(path));
     int i = 0;
     int found = 0;
     for(; i<strlen(path); i++){
         if(path[i]=='/') found++;
         if (found==level) {i++; break;}
     }
-    //printf("i %lu\n",strlen(path)-i+1);
     char* name = calloc(sizeof(char*)*(strlen(path)-i+1), 1);
     int temp = i;
     for(int j=0;j<(strlen(path)-i); j++) {name[j] = path[temp]; temp++;}
-    //printf("name %s\n",name);
     return name;
 } 
 
@@ -294,7 +235,6 @@ int* newFile(FILE* fp, char* contents, char* path, int type, unsigned char* free
         
             // Add blocks numbers to a list/array and then search through all of them in block storage space
             // see if block can be allocated for directory/file if it does not exist
-            //int* freeBlocks = checkFreeBlockBuffer(freeBlockBuffer, fileBlocks);
             blocksPositions = checkFreeBlockBuffer(freeBlockBuffer, fileBlocks+3);
             for (int k=0; k<fileBlocks+3; k++) flipBlockBuffer(freeBlockBuffer, blocksPositions[k]);
             // Check if Inode can be created before creating file, create file and then create inode passing in block_locations[] (int pointer)?
@@ -304,16 +244,10 @@ int* newFile(FILE* fp, char* contents, char* path, int type, unsigned char* free
             createNewFlatFile(contents, name, newInode, writeBuffer, 0);
         }
         
-        //printf("dir loc: %d\n", parentRef);
         addToDirectory(newInode, parentRef, 32, name, writeBuffer+((fileBlocks+1)*BLOCK_SIZE), checkBuffer, blocksPositions, fileBlocks, freeBlockBuffer); // Need to figure out how to updates directory sizes (in the inode)
         readBlock(fp, (int)*(checkBuffer+(parentNode*4)), writeBuffer+((fileBlocks+2)*BLOCK_SIZE)); // Read in old inode for updated directory
         memcpy(writeBuffer+((fileBlocks+2)*BLOCK_SIZE)+8, &blocksPositions[fileBlocks+1], 2); // Update inode of updated directory
         memcpy(checkBuffer+(parentNode*4), &blocksPositions[fileBlocks+2], 4); // Update the checkpoint location for new copy of inode
-
-        //else addToDirectory(newInode, (int)*(checkBuffer+((exists-1)*INODE_SIZE)+8), (int)*(checkBuffer+((exists-1)*INODE_SIZE)), name); 
-        
-        //printf("\ndir containter: %st%d\n",name, (int)*(checkBuffer+((exists-1)*INODE_SIZE)+8));
-
         return blocksPositions;
 }
 
@@ -367,11 +301,9 @@ int* deleteDirectory(FILE* fp, unsigned char* checkBuffer, unsigned char* freeBl
     readBlock(fp, parentRef, rootDir);
     for (int i=1; i>16; i++) if (*(rootDir+(i*32))!=0) {printf("Error: Directory contains files\n"); return buffBlocks;} 
     flipBlockBuffer(freeBlockBuffer, parentRef);
-    //flipBlockBuffer(freeBlockBuffer, parentNode); // Not freeing entire checkpoint block
     flipBlockBuffer(freeBlockBuffer, (int)*(checkBuffer+(parentNode*4))); //inode
     int clear = 0;
 
-    ///
     buffBlocks = checkFreeBlockBuffer(freeBlockBuffer, 2); // One block for updated directory and one for its inode
 
     unsigned char* tempBuffer = calloc(BLOCK_SIZE, 1);
@@ -381,7 +313,6 @@ int* deleteDirectory(FILE* fp, unsigned char* checkBuffer, unsigned char* freeBl
 
     int i = 1;
     for(;i<16;i++) if ((int)*(rootDir+(i*32))==parentNode) break;
-    //printf("FOUND %d\n",(int)*(rootDir+(i*32)));
  
     memcpy(rootDir+(i*32), &clear, 1); // readDirectory only checks first byte, if 0 then it skips so setting 0 should free the directory entry
     memcpy(writeBuffer, rootDir, BLOCK_SIZE);
@@ -429,7 +360,6 @@ int* deleteFile(FILE* fp, unsigned char* checkBuffer, unsigned char* freeBlockBu
    
     int clear = 0;
     memcpy(rootDir+(i*32), &clear, 1); // readDirectory only checks first byte, if 0 then it skips so setting 0 should free the directory entry
-    //memset(rootDir+(i*32), 0, 32);
     memcpy(writeBuffer, rootDir, BLOCK_SIZE);
     
     // Updating parent directory inode
@@ -464,10 +394,8 @@ int* updateFile(FILE* fp, unsigned char* checkBuffer, unsigned char* freeBlockBu
     char* temp = calloc(BLOCK_SIZE, 1);
 
     for (int i=0; i<blocksRequired+1; i++) {
-        //if (*(rootDir+8+(i*2))==0) break;
         readBlock(fp, (int)*(rootDir+8+(i*2)), oldBuffer);
         memcpy(temp, contents+(BLOCK_SIZE*i), BLOCK_SIZE);
-        //printf("THINGS:  %s\n\n\n%s\n", oldBuffer,contents+(BLOCK_SIZE*i));
         if (strcmp((char*)oldBuffer, temp)==0) {
             sameBlock++;
             continue; // If current block is the same as updated version, it doesn't need to be changed
@@ -475,11 +403,9 @@ int* updateFile(FILE* fp, unsigned char* checkBuffer, unsigned char* freeBlockBu
         else { // If updated block is differnet, record it to the buffer
             strncpy((char*)writeBuffer+(updatedBlock*BLOCK_SIZE), contents+(BLOCK_SIZE*i), BLOCK_SIZE);
             newBlocks[updatedBlock] = i;//*(rootDir+8+(i*2)); // If any blocks in updated contents are the same as on disk, record them
-            //flipBlockBuffer(freeBlockBuffer, *(rootDir+8+(i*2)));
             updatedBlock++;
         }
     }
-    //printf("SAME BLOCK %d\t BLOCKS REQUIED: %d\tUPDATED BLCOKS%d\n",sameBlock, blocksRequired, updatedBlock);
  
     buffBlocks = checkFreeBlockBuffer(freeBlockBuffer, blocksRequired+1); // Allocate new blocks for each updated block plus the inode
     int i=sameBlock-1;
@@ -490,13 +416,11 @@ int* updateFile(FILE* fp, unsigned char* checkBuffer, unsigned char* freeBlockBu
         flipBlockBuffer(freeBlockBuffer, buffBlocks[i]); 
     }
     flipBlockBuffer(freeBlockBuffer, buffBlocks[blocksRequired]);
-   //for (int k=0; k<blocksRequired; k++) { // Indicate the newly allocated blocks as occupied
     int clear = 0;
     for (;i<10; i++) memcpy(rootDir+8+(i*2), &clear, 2); // Clear out old unused references when new file is smaller then the old
 
     memcpy(checkBuffer+(parentNode*4), &buffBlocks[blocksRequired], 4); // Update the inode checkpoint
     memcpy(writeBuffer+(blocksRequired*BLOCK_SIZE), rootDir, BLOCK_SIZE); // Copy updated inode to end of buffer
-    //printf("Inode stats %d\n", (int)*(rootDir));
     free(oldBuffer);
     free(newBuffer);
     return buffBlocks;
@@ -551,7 +475,7 @@ void buffControl(char* arg, char* name, char* contents) {
         fileBlocks = -1;
     }
 
-    else if (strcmp(arg, "set")==0) { // Change this later to check if file already exists, run this if it does
+    else if (strcmp(arg, "set")==0) {
         fileBlocks = ceil((double)strlen(contents)/(double)BLOCK_SIZE);
         writeBuffer = calloc(BLOCK_SIZE, fileBlocks+1);
         blocksPositions = updateFile(fp, checkBuffer, freeBlockBuffer, dirBuffer, name, blocksPositions, writeBuffer, contents);
@@ -568,8 +492,6 @@ void buffControl(char* arg, char* name, char* contents) {
         writeBlock(fp, blocksPositions[i], writeBuffer+(i*BLOCK_SIZE));
     }
     
-    //for(int i=0; i<fileBlocks+3; i++) printf("Blcoks: %d\n", blocksPositions[i]);
-
     free(freeBlockBuffer);
     free(checkBuffer);
     free(dirBuffer);
